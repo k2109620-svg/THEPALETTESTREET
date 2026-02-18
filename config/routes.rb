9cbeec99f71f2c2d6393f0df0e960f0cf19ac494
k2109620-svg <k2109620@gmail.com>
2026-02-19 07:00:14 +0900

    aaaaaaaaaaaaaaaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+  sessions: 'users/sessions'
+}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,12 +15,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :tweets
-  get "home", to: "home#index"
   get "lives/index"
   get "lives/show"
 
   resources :users, only: [:show], path: "members"
-  resources :tweets
   root 'home#index'
   get "home", to: "home#index"
 
