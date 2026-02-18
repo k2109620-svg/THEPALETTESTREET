672d368399d433f0827537a1c2195ec4d5d975e9
k2109620-svg <k2109620@gmail.com>
2026-02-19 07:11:21 +0900

    avcdefg

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,20 +1,25 @@
 class Users::SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate(auth_options)
-
-    # ログイン失敗（メール or パスワードが違う）
+    # ログイン失敗
     return super unless resource
-
     # 管理者チェック
     unless resource.admin?
-      sign_out resource
+      # ここで sign_out は使わない（Turbo が嫌う）
+      reset_session
+
       flash[:alert] = "管理者のみログインできます"
-      redirect_to new_user_session_path and return
+
+      respond_to do |format|
+        format.html { redirect_to new_user_session_path }
+        format.turbo_stream { redirect_to new_user_session_path }
+      end
+
+      return
     end
 
     # 管理者なら通常ログイン
     super
   end
-respond_to :html, :turbo_stream
+  respond_to :html, :turbo_stream
 end
-
